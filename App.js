b5b04d88fccf311d4fe702997c2e8cb92461b954
David Faith <faithodesola@gmail.com>
2021-10-09 14:14:43 +0100

    Initializing...

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Signin from "./screens/Signin";
+import Signup from "./screens/Signup";
+import Home from "./screens/Home";
+import GameDetails from "./screens/GameDetails";
+import Profile from "./screens/Profile";
+import BottomTabNavigation from "./navigation/BottomTabNavigation";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Sign in"
+          component={Signin}
+          options={{
+            header: () => null,
+            navigationOptions: {
+              headerShown: false,
+            },
+            headerStyle: {
+              backgroundColor: "#000000",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Sign up"
+          component={Signup}
+          options={{
+            header: () => null,
+            navigationOptions: {
+              headerShown: false,
+            },
+            headerStyle: {
+              backgroundColor: "#000000",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={BottomTabNavigation}
+          options={{
+            header: () => null,
+            navigationOptions: {
+              headerShown: false,
+            },
+            headerStyle: {
+              backgroundColor: "#000000",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
